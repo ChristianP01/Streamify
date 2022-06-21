a7f8f42aff6aa90f2218d1ca00bad900b18a3c85
ChristianP01 <christianpreti01@gmail.com>
2022-06-21 22:23:12 +0200

    Test added

--- streamify/tests.py
+++ streamify/tests.py
@@ -1,3 +1,23 @@
-from django.test import TestCase
+from urllib import request
+from django.http import HttpRequest, HttpResponse
+from django.test import Client, TestCase
+from my_auth.views import logged
+from streamify.models import Utente
 
-# Create your tests here.
+class TestNewUser(TestCase):
+
+    def setUp(self):
+        test_user = Utente.objects.create(
+            username = "TestUser",
+            nome = "Test",
+            cognome = "User",
+            password = "Test",
+            email = "test@test.com"
+        )
+
+        c = Client()
+        response = c.post('/auth/accedi/', {'uname': 'ChristianP01', 'psw': 'gg'})
+        print(response)
+    
+    def test_creaUser(self):
+        self.assertEqual(Utente.objects.count(), 1)

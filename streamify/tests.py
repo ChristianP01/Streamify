844bc805ba3696b2b84348ece5e7f95084a0bdd2
ChristianP01 <christianpreti01@gmail.com>
2022-06-21 22:28:25 +0200

    fix

--- streamify/tests.py
+++ streamify/tests.py
@@ -1,16 +1,30 @@
-from urllib import request
+from urllib import request, response
 from django.http import HttpRequest, HttpResponse
 from django.test import Client, TestCase
 from my_auth.views import logged
 from streamify.models import Utente
 
-class TestNewUser(TestCase):
+class TestRegistrati(TestCase):
+
+    def setUp(self):
+        """Testa la corretta registrazione di un utente (esistente)"""
+        c = Client()
+        self.response = c.post('/auth/registrati/', {
+            'uname': 'testUser',
+            'psw': 'test',
+            'email': 'test@test.it',
+            'nome': 'Test',
+            'cognome': 'test'})
+    
+    def test_registraUser(self):
+        self.assertEqual(self.response.status_code, 200)
+
+class TestLogin(TestCase):
 
     def setUp(self):
         """Testa il corretto login di un utente (esistente)"""
         c = Client()
-        response = c.post('/auth/accedi/', {'uname': 'ChristianP01', 'psw': 'gg'})
-        print(response)
+        self.response = c.post('/auth/accedi/', {'uname': 'ChristianP01', 'psw': 'gg'})
     
-    def test_creaUser(self):
-        self.assertEqual(Utente.objects.count(), 1)
+    def test_loginUser(self):
+        self.assertEqual(self.response.status_code, 200)

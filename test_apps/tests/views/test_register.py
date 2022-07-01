2b7bdac30400bd43c4a839a5505147e7cc54ad60
ChristianP01 <christianpreti01@gmail.com>
2022-07-01 10:37:09 +0200

    New test added

--- test_apps/tests/views/test_register.py
+++ test_apps/tests/views/test_register.py
@@ -20,7 +20,7 @@ class TestRegisterSuccess(TestCase):
 
 
 
-class TestRegisterFail(TestCase):
+class TestRegisterFailConflictUsername(TestCase):
 
     def setUp(self):
 
@@ -33,14 +33,39 @@ class TestRegisterFail(TestCase):
 
         creds = {
             'username' :'testUser',
-            'password' : 'testUser',
-            'email' : 'test@test.it',
+            'password' : 'anotherPsw',
+            'email' : 'anothermail@email.it',
             'nome' : 'Test',
             'cognome' : 'test'}
 
         self.client = Client()
         self.response = self.client.post('/auth/registrati/', creds)
         
-    def test_register_fail(self):
+    def test_register_fail_conflict_username(self):
+        self.assertEqual(self.response.status_code, 409)
+
+
+class TestRegisterFailConflictEmail(TestCase):
+
+    def setUp(self):
+
+        test_user = Utente.objects.create(
+            username='testUser',
+            password='testUser',
+            email='test@test.it',
+            nome='Test',
+            cognome='test')
+
+        creds = {
+            'username' :'anotherUser',
+            'password' : 'anotherPsw',
+            'email' : 'test@test.it',
+            'nome' : 'anotherName',
+            'cognome' : 'anotherSurname'}
+
+        self.client = Client()
+        self.response = self.client.post('/auth/registrati/', creds)
+        
+    def test_register_fail_conflict_email(self):
         self.assertEqual(self.response.status_code, 409)
 
